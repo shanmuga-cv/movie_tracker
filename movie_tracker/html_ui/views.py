dcc52b37128da18fd02d223560b193568f1e43bd
shanmuga <shanmuga.chidambaravel@gmail.com>
2015-12-13 00:55:14 +0530

    supported users

--- movie_tracker/html_ui/views.py
+++ movie_tracker/html_ui/views.py
@@ -6,32 +6,41 @@ from ..model.model import ConnectionManager, Movie, MovieWatchers
 from time import sleep
 
 session = ConnectionManager.session
-
-# @view_config(route_name='home', renderer='templates/movies.jinja2')
-@view_config(route_name='movie_list')
-def list_movies(request):
+global_render_dict = {'project_name': 'Movie Tracker'}
+def get_render_dict(request):
     current_user = None
     if (request.cookies.get('user_id')):
         current_user_id = request.cookies['user_id']
         current_user = session.query(MovieWatchers).filter(MovieWatchers.user_id == current_user_id).one()
+    render_dict = dict(global_render_dict, current_user=current_user)
+    return render_dict
+
+# @view_config(route_name='home', renderer='templates/movies.jinja2')
+@view_config(route_name='movie_list')
+def list_movies(request):
+    render_dict = get_render_dict(request)
     movies = session.query(Movie).limit(100).all()
     print(len(movies))
     total_movies = session.query(Movie).count()
-    template_html = render('templates/movies.jinja2',
-                      {'project_name':'movie_tracker', 'movie_count': total_movies, 'moveis':movies, 'current_user':current_user})
+    render_dict['movie_count'] = total_movies
+    render_dict['movies'] = movies
+    template_html = render('templates/movies.jinja2', render_dict)
     return Response(template_html)
 
 @view_config(route_name="movie_details")#, renderer="templates/movie_details.jinja2")
 def movie_detail(request):
+    render_dict = get_render_dict(request)
     movie = session.query(Movie).filter(Movie.movie_id == request.matchdict['movie_id']).one()
-    render_dict = dict(request.matchdict, **{'project_name':'Movie Tracker', 'movie':movie})
+    render_dict['movie'] = movie
     template_html = render("templates/movie_details.jinja2", render_dict)
     return Response(template_html)
 
 @view_config(route_name="user_list")
 def listUsers(request):
+    render_dict = get_render_dict(request)
     users = session.query(MovieWatchers).all()
-    template_html = render('templates/users.jinja2', {'users':users, 'project_name':'Movie Tracker'})
+    render_dict['users'] = users
+    template_html = render('templates/users.jinja2', render_dict)
     return Response(template_html)
 
 @view_config(route_name="select_user")
