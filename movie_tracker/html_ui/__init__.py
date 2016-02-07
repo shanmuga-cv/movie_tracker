7b15f9796cee11d40d1b0b83aa38141c549c8cc2
Shanmuga <shanmuga.chidambaravel@gmail.com>
2016-02-07 18:11:52 +0530

    list movies seen by all

--- movie_tracker/html_ui/__init__.py
+++ movie_tracker/html_ui/__init__.py
@@ -12,6 +12,7 @@ def main():
 
     config.add_route('movie_list', '/show_movie')
     config.add_route('movie_json', '/movies')
+    config.add_route('movies_seen_by_all', '/movies/seen_by_all')
     config.add_route('get_movie_by_id', '/movie/get/{movie_id}', request_method='GET')
     config.add_route('movie_details', '/movie/{movie_id}')
 
--- movie_tracker/html_ui/views.py
+++ movie_tracker/html_ui/views.py
@@ -7,6 +7,7 @@ from urllib import request as url_request
 from pyramid.renderers import render
 from pyramid.response import Response, FileResponse
 from pyramid.view import view_config
+from sqlalchemy import func, distinct
 
 from ..model.model import ConnectionManager, Movie, MovieWatchers, MovieViewings, DirMonitor, ConfigManager
 
@@ -125,6 +126,15 @@ def scan(request):
 def delete(request):
     render_dict = get_render_dict(request)
     return Response(render('/templates/delete.jinja2', render_dict))
+
+
+@view_config(route_name='movies_seen_by_all')
+def movies_seen_by_all(request):
+    total_users = session.query(MovieWatchers).count()
+    movies_ids_seen_by_all = session.query(MovieViewings.movie_id).group_by(MovieViewings.movie_id).having(
+        func.count(distinct(MovieViewings.user_id)) == total_users)
+    movies_seen_by_all = session.query(Movie).filter(Movie.movie_id.in_(movies_ids_seen_by_all)).all()
+    return Response(json.dumps([movie.make_dict() for movie in movies_seen_by_all]))
 
 
 @view_config(route_name='delete_watched_by_all')
